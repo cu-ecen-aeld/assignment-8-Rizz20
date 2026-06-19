f97071ace59ef3b199772fb929d1156240e2eb72
Rizz20 <rizulsharma2005@gmail.com>
2026-06-19 13:32:22 +0530

    Update aesdsocket commit hash to include error handling fixes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '4dee6f2de9ece7d56638314f32a292a870d2ed6e'
+AESD_ASSIGNMENTS_VERSION = '0db287b252b53aebd582976ba57888c96f4fcc92'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
